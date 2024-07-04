2a8c87f6e94cf9ae40860d224cc947051c76fd00
Paulina-Pereyra <paulinapereyrasantana@gmail.com>
2024-07-04 14:32:44 +0000

    proyecto carta poker terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  const carta = document.querySelector(".carta");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  carta.classList.add(generateRandomPalo());
+  carta.innerHTML = generateRandomNumber();
+};
+
+const generateRandomNumber = () => {
+  const numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const indexNumeros = Math.floor(Math.random() * numeros.length);
+  return numeros[indexNumeros];
+};
+
+const generateRandomPalo = () => {
+  const palos = ["pica", "trébol", "diamante", "corazón"];
+  const indexPalos = Math.floor(Math.random() * palos.length);
+  return palos[indexPalos];
 };
